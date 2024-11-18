6092cc84b4af00d0980e1a84baf066e826cac2d4
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-11-18 21:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Create an object called student
+const student = {
+  name: "John Doe"
+};
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
